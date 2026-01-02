c70826f904941215efff7a5a5c650286c16bf5a2
Vinay372005 <ee24b006@iittp.ac.in>
2026-01-02 13:13:14 +0530

    Update train.py

--- app/train.py
+++ app/train.py
@@ -1,1 +1,15 @@
 # Training script (to be implemented)
+import os
+
+CLAP_DIR = "data/clap"
+NOISE_DIR = "data/noise"
+
+def check_data():
+    clap_files = os.listdir(CLAP_DIR)
+    noise_files = os.listdir(NOISE_DIR)
+
+    print("Clap files:", len(clap_files))
+    print("Noise files:", len(noise_files))
+
+if __name__ == "__main__":
+    check_data()
