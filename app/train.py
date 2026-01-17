3cbd0e0a69e3da80cbcc03abef722e466c5af208
Vinay372005 <ee24b006@iittp.ac.in>
2026-01-17 20:44:33 +0530

    Update train.py

--- app/train.py
+++ app/train.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 from app.features import extract_features
@@ -17,23 +16,28 @@ for file in os.listdir(CLAP_DIR):
     if file.endswith(".wav"):
         path = os.path.join(CLAP_DIR, file)
         X.append(extract_features(path))
-        y.append(1)
+        y.append(1)  # Clap = 1
 
 print("Processing NOISE files...")
 for file in os.listdir(NOISE_DIR):
     if file.endswith(".wav"):
         path = os.path.join(NOISE_DIR, file)
         X.append(extract_features(path))
-        y.append(0)
+        y.append(0)  # Noise = 0
 
 X = np.array(X)
 y = np.array(y)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
+
 model.fit(X, y)
 
 os.makedirs("models", exist_ok=True)
 joblib.dump(model, MODEL_PATH)
 
-print("✅ Model trained successfully")
-print("Model saved to:", MODEL_PATH)
+print("✅ TRAINING COMPLETE")
+print("Model saved at:", MODEL_PATH)
+print("Samples:", len(y))
