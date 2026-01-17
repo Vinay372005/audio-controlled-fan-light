e4474c94a8e385939f4339032007eb53f9604d16
Vinay372005 <ee24b006@iittp.ac.in>
2026-01-17 20:35:26 +0530

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,20 +1,20 @@
-import os
-import joblib
+import sys
 import numpy as np
+import joblib
 from app.features import extract_features
 
 MODEL_PATH = "models/clap_detector.pkl"
 
-def predict(file_path):
+def predict(audio_file):
     model = joblib.load(MODEL_PATH)
-
-    features = extract_features(file_path)
+    features = extract_features(audio_file)
     features = np.array(features).reshape(1, -1)
 
     prediction = model.predict(features)[0]
-    return prediction
+    return "CLAP" if prediction == 1 else "NOISE"
+
 
 if __name__ == "__main__":
-    test_file = "data/clap/clap_01.wav"
-    result = predict(test_file)
+    audio_path = sys.argv[1]
+    result = predict(audio_path)
     print("Prediction:", result)
