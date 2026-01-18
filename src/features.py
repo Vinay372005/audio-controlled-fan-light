9db64314bd3912733dfc4e392a434e8a07b38a28
Vinay372005 <ee24b006@iittp.ac.in>
2026-01-18 09:05:10 +0530

    Delete src directory

--- src/features.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import librosa
-
-def extract_features(file_path):
-    y, sr = librosa.load(file_path, sr=None)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-    return np.mean(mfccs.T, axis=0)
